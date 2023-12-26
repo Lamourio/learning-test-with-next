7727eb773fd5c8dca772435c5e0bd66860b7866d
Marwane lamouri <marwane.lamouri@cxg.com>
2023-12-26 17:03:46 +0100

    behavior

--- __tests__/HomePage.test.jsx
+++ __tests__/HomePage.test.jsx
@@ -1,30 +1,55 @@
 import Home from "@/app/page";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
+describe('Home page',() => {
+        describe('rendering', () => {
+            it('should have Home page text', () => {
+                render(<Home />);
+                expect(screen.getByText('Home Page')).toBeInTheDocument();
+            })
+        
+            it('should have a button with a text click me', () => {
+                render(<Home />);
+                expect(screen.getByRole('button',{name : 'click me'})).toBeInTheDocument();
+            })
+        
+            it('should have input field with label Enter Random Text', () => {
+                render(<Home />);
+                expect(screen.getByLabelText(/Enter Random/)).toBeInTheDocument();
+            })
+        
+            it('should have input field with a place holder text search', () => {
+                render(<Home />);
+                expect(screen.getByPlaceholderText(/Search/)).toBeInTheDocument();
+            })
+        
+            it('should have input field with a place holder text search', () => {
+                render(<Home />);
+                expect(screen.getByDisplayValue(/AUDI/)).toBeInTheDocument();
+            })
+        
+            it('should not find element with text: this is the text!', () => {
+                render(<Home  />)
+                expect(screen.queryByText("This is the text!")).not.toBeInTheDocument();
+                
+            })
+        })
 
-describe('Home page - rendering',() => {
-    it('should have Home page text', () => {
-        render(<Home />);
-        expect(screen.getByText('Home Page')).toBeInTheDocument();
-    })
+        describe('Behavior',() => {
+            it('should click on show button and find new text', async () => {
+                render(<Home />);
+                expect(screen.queryByText("This is the text!")).not.toBeInTheDocument();
+                const showTextButton = screen.getByRole('button',{name : "Show Text"});
+                await userEvent.click(showTextButton);
+                // expect(screen.getByText("This is the text!")).toBeInTheDocument();
+                // findBy* will throw a error after 1000ms
+                // expect(await screen.findByText('This is the text!',{},{timeout : 5000})).toBeInTheDocument();
 
-    it('should have a button with a text click me', () => {
-        render(<Home />);
-        expect(screen.getByRole('button',{name : 'click me'})).toBeInTheDocument();
-    })
+                await waitFor(() => {
+                    expect(screen.getByText("This is the text!")).toBeInTheDocument();
+                },{timeout : 3000})
+            })
+        });
+});
 
-    it('should have input field with label Enter Random Text', () => {
-        render(<Home />);
-        expect(screen.getByLabelText(/Enter Random/)).toBeInTheDocument();
-    })
-
-    it('should have input field with a place holder text search', () => {
-        render(<Home />);
-        expect(screen.getByPlaceholderText(/Search/)).toBeInTheDocument();
-    })
-
-    it('should have input field with a place holder text search', () => {
-        render(<Home />);
-        expect(screen.getByDisplayValue(/AUDI/)).toBeInTheDocument();
-    })
-})
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,27 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import { useState } from "react"
 
 export default function Home() {
+  const [showText, setShowText] = useState(false);
   return (
-    <main className={styles.main}>
+    <main>
       <h1>Home Page</h1>
+      <button>click me</button>
+      <div>
+        <label htmlFor="randomText">Enter Random Text:</label>
+        <input id="randomText"/>
+      </div>
+      
+      <div>
+        <input placeholder="Search..."/>
+      </div>
+      <div>
+        <input defaultValue="AUDI Q5"/>
+      </div>
+      <div>
+        {showText && <span>This is the text!</span> }
+        <button onClick={() => setTimeout(() => {setShowText(s => !s)},1500) }>Show Text</button>
+      </div>
     </main>
   )
 }
